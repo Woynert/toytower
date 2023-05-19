4cd11f5562456a7d5344097e26208f76b12d9fa2
woynert <whayner.porras.2020@upb.edu.co>
2023-05-19 14:35:44 -0500

    feat(player): Basic FPS controller

---
+++ scenes/player/player.gd
@@ -0,0 +1,63 @@
+extends CharacterBody3D
+
+@onready var head = $head as Node3D
+@onready var camera = $head/camera as Camera3D
+@onready var gunRay = $head/camera/gunRay as RayCast3D
+
+var mouseSensibility = 800
+var mouseRelativeX = 0
+var mouseRelativeY = 0
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+const GRAVITY = 10
+
+
+func _ready():
+	# stops rgun from hitting yourself
+	gunRay.add_exception(self)
+	
+	# captures mouse 
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+func _physics_process(delta):
+	
+	# gravity
+	if not is_on_floor():
+		velocity.y -= GRAVITY * delta
+
+	# jump
+	if Input.is_action_just_pressed("gm_jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	# shoot
+	if Input.is_action_just_pressed("gm_primary_action"):
+		shoot()
+		
+	# get the input direction and handle the movement / deceleration.
+	var input_dir = Input.get_vector("gm_left", "gm_right", "gm_up", "gm_down")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		
+		head.rotation.y -= event.relative.x / mouseSensibility
+		camera.rotation.x -= event.relative.y / mouseSensibility
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90) )
+		mouseRelativeX = clamp(event.relative.x, -50, 50)
+		mouseRelativeY = clamp(event.relative.y, -50, 10)
+
+func shoot():
+	if not gunRay.is_colliding():
+		return
+	
+	print(gunRay.get_collision_point())
+	print(gunRay.get_collision_point()+gunRay.get_collision_normal())
