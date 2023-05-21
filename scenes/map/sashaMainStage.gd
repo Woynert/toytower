d453984f318233997daf05e652273a663e96a4f5
woynert <whayner.porras.2020@upb.edu.co>
2023-05-20 20:04:24 -0500

    fix(ui): Fix main screen Sasha animation bumps

--- scenes/map/sashaMainStage.gd
+++ scenes/map/sashaMainStage.gd
@@ -6,10 +6,11 @@ var smHead: AnimationNodeStateMachinePlayback
 func _ready():
 	rng.randomize()
 	smHead = $AnimationTree.get("parameters/StateMachine/playback")
+	smHead.start("posing2")
 	
 
 # randomly play an animation
 func _on_timer_timeout():
 	
-	var animation = "posing" + str(rng.randi_range(1, 2))
-	smHead.start(animation)
+	var animation = "posing" + str(rng.randi_range(0, 2))
+	smHead.travel(animation)
