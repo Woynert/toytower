d4cef61bc49e272a520a1768070444290a3e2369
woynert <whayner.porras.2020@upb.edu.co>
2023-05-20 23:18:34 -0500

    feat(ui): Damage billiboard

---
+++ scenes/common/gizmoBilliboard.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+var speed = 0.02
+var speedAlpha = 0.015
+
+func setup(text: String, color: Color = Color.WHITE):
+	$Label3D.text = text
+	$Label3D.modulate = color
+	
+func _physics_process(delta):
+	$Label3D.global_position.y += speed
+	$Label3D.transparency += speedAlpha
+	
+	if $Label3D.transparency >= 1:
+		queue_free()
+	
+	
